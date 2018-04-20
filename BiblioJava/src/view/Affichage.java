5fdc9c1c019d475562cad7412d3fd40db12d5553
Kilian POULIN <kilian.poulin@efrei.net>
2018-04-20 13:00:36 +0200

    Javadoc

--- BiblioJava/src/view/Affichage.java
+++ BiblioJava/src/view/Affichage.java
@@ -4,15 +4,18 @@ import java.util.Collection;
 import model.Document;
 
 /**
- *
+ *  Cette classe contient des méthodes servant à l'affichage d'une partie d'une bibliothèque
+ *  (un certain nombre de documents).
  * @author Kilian
  */
 public interface Affichage {
 
     /**
-     *
+     * Convertie l'affichage d'une liste de documents en string.
      * @param docs
-     * @return
+     *      La liste de documents à afficher
+     * @return 
+     *      un élement de type String affichant plusieurs documents.
      */
     public static String afficherDocument(Collection<Document> docs){
             StringBuilder biblio = new StringBuilder();
